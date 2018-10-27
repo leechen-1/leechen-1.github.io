b25270ced9c1c57d6d92592ddea501d92af0b1d9
leechen <33708946+leechen-1@users.noreply.github.com>
2018-10-27 13:14:12 +0800

    Add files via upload

---
+++ introduction/Android/Android1/01Java SE/01/codes/Hello.java
@@ -0,0 +1,22 @@
+//定义一个类，类名为Hello 
+
+/*
+
+	多行注释内容
+	
+		A
+	
+*/
+
+/**
+	文档注释
+	
+*/
+class  Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("你好师姐");
+    }
+}
+
