a73e71caaaa38f658e4404cfddf1070e2d54535e
leechen <33708946+leechen-1@users.noreply.github.com>
2018-10-27 15:46:24 +0800

    Add files via upload

---
+++ introduction/JavaEE/guolei/03Spring/Source/15-buyStock-txAnnotation/src/com/abc/dao/AccountDaoImpl.java
@@ -0,0 +1,22 @@
+package com.abc.dao;
+
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+public class AccountDaoImpl extends JdbcDaoSupport implements IAccountDao {
+
+	@Override
+	public void insertAccount(String aname, double money) {
+		String sql = "insert into account(aname,balance) values(?,?)";
+		this.getJdbcTemplate().update(sql, aname, money);
+	}
+
+	@Override
+	public void updateAccount(String aname, double money, boolean isBuy) {
+		String sql = "update account set balance=balance+? where aname=?";
+		if(isBuy) {
+			sql = "update account set balance=balance-? where aname=?";
+		}
+		this.getJdbcTemplate().update(sql, money, aname);
+	}
+
+}
