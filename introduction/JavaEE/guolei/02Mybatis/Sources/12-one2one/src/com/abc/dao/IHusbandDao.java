21687abe059c674936e0fb2a698135e22bd37bca
leechen <33708946+leechen-1@users.noreply.github.com>
2018-10-27 15:04:25 +0800

    Add files via upload

---
+++ introduction/JavaEE/guolei/02Mybatis/Sources/12-one2one/src/com/abc/dao/IHusbandDao.java
@@ -0,0 +1,10 @@
+package com.abc.dao;
+
+import com.abc.beans.Husband;
+
+public interface IHusbandDao {
+	Husband selectHusbandById(int hid);
+}
+
+
+
---
+++ introduction/JavaEE/guolei/02Mybatis/Sources/12-one2one/src/com/abc/test/MyTest.java
@@ -0,0 +1,63 @@
+package com.abc.test;
+
+import java.io.IOException;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.abc.beans.Husband;
+import com.abc.dao.IHusbandDao;
+import com.abc.utils.MybatisUtil;
+public class MyTest {
+
+	private IHusbandDao dao;
+	private SqlSession session;
+
+	@Before
+	public void before() throws IOException {
+		session = MybatisUtil.getSession();
+		dao = session.getMapper(IHusbandDao.class);
+	}
+	
+	@After
+	public void after() {
+		if(session != null) {
+			session.close();
+		}
+	}
+	
+	@Test
+	public void test01() {
+		Husband husband = dao.selectHusbandById(2);
+		System.out.println(husband);
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
