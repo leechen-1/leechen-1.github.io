1d3f20aa8b9e01d86c797a492b0991d73a5dd2b0
leechen <33708946+leechen-1@users.noreply.github.com>
2018-10-27 16:20:11 +0800

    Add files via upload

---
+++ introduction/JavaEE/guolei/04SpringMVC/Resources/10-simpleUrlHandlerMapping/src/com/abc/handlers/SomeHandler.java
@@ -0,0 +1,18 @@
+package com.abc.handlers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+// 定义处理器
+public class SomeHandler implements Controller {
+
+	
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		return new ModelAndView("/welcome.jsp");
+	}
+
+}
