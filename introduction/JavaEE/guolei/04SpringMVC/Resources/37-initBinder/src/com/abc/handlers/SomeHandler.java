e1ea668044f9cbedde1e1578e64ad2f31065de65
leechen <33708946+leechen-1@users.noreply.github.com>
2018-10-27 16:50:09 +0800

    Add files via upload

---
+++ introduction/JavaEE/guolei/04SpringMVC/Resources/37-initBinder/src/com/abc/handlers/SomeHandler.java
@@ -0,0 +1,59 @@
+package com.abc.handlers;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+// 定义处理器
+@Controller
+@RequestMapping("/some")  
+public class SomeHandler {
+
+	@RequestMapping("/register.do")
+	public ModelAndView doRegister(int age, Date birthday) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("age", age);
+		mv.addObject("birthday", birthday);
+		mv.setViewName("/WEB-INF/jsp/welcome.jsp");
+		return mv;
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(sdf, true));
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
