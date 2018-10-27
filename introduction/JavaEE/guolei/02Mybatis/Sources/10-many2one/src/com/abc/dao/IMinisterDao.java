b2e3d013a07e91431c5535d3b697950246910dea
leechen <33708946+leechen-1@users.noreply.github.com>
2018-10-27 15:00:18 +0800

    Add files via upload

---
+++ introduction/JavaEE/guolei/02Mybatis/Sources/10-many2one/src/com/abc/dao/IMinisterDao.java
@@ -0,0 +1,10 @@
+package com.abc.dao;
+
+import com.abc.beans.Minister;
+
+public interface IMinisterDao {
+	Minister selectMinisterById(int mid);
+}
+
+
+
---
+++ introduction/JavaEE/guolei/02Mybatis/Sources/10-many2one/src/com/abc/test/MyTest.java
@@ -0,0 +1,64 @@
+package com.abc.test;
+
+import java.io.IOException;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.abc.beans.Country;
+import com.abc.beans.Minister;
+import com.abc.dao.IMinisterDao;
+import com.abc.utils.MybatisUtil;
+public class MyTest {
+
+	private IMinisterDao dao;
+	private SqlSession session;
+
+	@Before
+	public void before() throws IOException {
+		session = MybatisUtil.getSession();
+		dao = session.getMapper(IMinisterDao.class);
+	}
+	
+	@After
+	public void after() {
+		if(session != null) {
+			session.close();
+		}
+	}
+	
+	@Test
+	public void test01() {
+		Minister minister = dao.selectMinisterById(2);
+		System.out.println(minister);
+	}
+	
+	
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
