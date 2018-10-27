14bb9f0010cefb43f9b96babcf0cf0b41771d2f7
leechen <33708946+leechen-1@users.noreply.github.com>
2018-10-27 15:09:47 +0800

    Add files via upload

---
+++ introduction/JavaEE/guolei/02Mybatis/Sources/17-oneself-one2many2/src/com/abc/beans/NewsLabel.java
@@ -0,0 +1,42 @@
+package com.abc.beans;
+
+import java.util.Set;
+
+// 新闻栏目
+// 现在将其看作是一方
+public class NewsLabel {
+	private Integer id;
+	private String name;
+	// 关联属性：多方
+	private Set<NewsLabel> children;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<NewsLabel> getChildren() {
+		return children;
+	}
+
+	public void setChildren(Set<NewsLabel> children) {
+		this.children = children;
+	}
+
+	@Override
+	public String toString() {
+		return "NewsLabel [id=" + id + ", name=" + name + ", children=" + children + "]";
+	}
+
+}
